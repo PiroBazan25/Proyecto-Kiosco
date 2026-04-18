af3e6961afb55e7aeefb1db212900dde8b50a328
Angel Bazán <piro2594@gmail.com>
2026-04-17 23:27:23 -0300

    Fix: use port 8080 for railway

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/api/ping', (req, res) => {
   res.json({ ok: true, mensaje: 'KioscoManager API funcionando' });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
